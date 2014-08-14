ab485c58af5011b129293410b1c778d5580d61b3
Gilbou <gilbert.val@gmail.com>
2014-08-14 15:19:04 +0200

    index show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Cocktail.create(name: "Mojito", description: "Très parfumé, légèrement sucré et avec une pointe d'acidité, ce Cocktail élégant et cosmopolite a fait sa place parmi les grands classiques et c'est aujourd'hui le Cocktail")
+Cocktail.create(name: "Margarita", description: "La Margarita est un cocktail à base de tequila, inventé par des Américains au Mexique.")
+Cocktail.create(name: "Sex on the beach", description: "Le Sex on the beach est un cocktail alcoolisé créé par le TGI Friday's")
+Cocktail.create(name: "Soupe de champagne", description: "La Soupe champenoise est un apéritif traditionnel de la Champagne")
+Cocktail.create(name: "Caiprinha", description: "La caipirinha est un cocktail brésilien préparé à base de cachaça, de sucre de canne et de citron vert.")
+
