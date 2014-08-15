cdabc604aa4aa34d763b5a1357cf75efa291217c
Gilbou <gilbert.val@gmail.com>
2014-08-15 16:33:36 +0200

    ajout de l upload photo + stockage S3

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,12 @@ class Cocktail < ActiveRecord::Base
   validates :name, presence: true
   has_many :doses
   has_many :ingredients, through: :doses
+
+  has_attached_file :picture,
+  styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :picture,
+  content_type: /\Aimage\/.*\z/
+
+
 end
